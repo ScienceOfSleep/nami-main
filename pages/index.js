7a7488a987d5099518cddf69658d4dba89dd5911
Josh <34256170+ScienceOfSleep@users.noreply.github.com>
2021-03-13 11:51:56 -0700

    Set up a static page with the data

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,34 @@
 import { connectToDatabase } from '../util/mongodb'
 
-export default function Home({isConnected}) {
+export default function Home({supports}) {
   return (
       <main>
         <h1>
           Nami Main
         </h1>
-        {isConnected ? (
-            <h2 className="subtitle">You are connected to MongoDB</h2>
-        ) : (
-            <h2 className="subtitle">
-                You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
-                for instructions.
-            </h2>
-        )}
+        <ul>
+            {supports.map((support) => (
+                <li>
+                    <h2>{support.name}</h2>
+                    <h3>MS: {support.ms}</h3>
+                    <p>AA: {support.range.aa} Q: {support.range.q} W: {support.range.w} E: {support.range.e} R: {support.range.r}</p>
+                    <p>{support.notes}</p>
+                </li>
+            ))}
+        </ul>
       </main>
   )
 }
 
-export async function getServerSideProps(context) {
-    const { client } = await connectToDatabase()
-
-    const isConnected = await client.isConnected()
-
+export async function getStaticProps() {
+    const { db } = await connectToDatabase();
+    const supports = await db
+        .collection("Supports")
+        .find({})
+        .toArray();
     return {
-        props: { isConnected },
-    }
+        props: {
+            supports: JSON.parse(JSON.stringify(supports)),
+        },
+    };
 }
