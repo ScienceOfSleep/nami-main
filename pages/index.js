779d6373712df8987d06a64eaecbcd64fba9bc2e
Josh <34256170+ScienceOfSleep@users.noreply.github.com>
2021-03-15 14:44:48 -0600

    Dropdown selector with useEffect

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,59 @@
 import { connectToDatabase } from '../util/mongodb'
+import {useState, useEffect} from "react"
 
 export default function Home({supports}) {
-  return (
+    const [ opponent, setOpponent ] = useState("Swain")
+    const [ opponentObject, setOpponentObject] = useState([])
+
+    useEffect(() => {
+        supports.map((support) => {
+            if (support.name === opponent){
+                setOpponentObject(support)
+            }
+        }),
+        [opponent, opponentObject]
+    })
+
+    return (
       <main>
         <h1>
           Nami Main
         </h1>
-        <ul>
-            {supports.map((support) => (
-                <li>
-                    <h2>{support.name}</h2>
-                    <h3>MS: {support.ms}</h3>
-                    <p>AA: {support.range.aa} Q: {support.range.q} W: {support.range.w} E: {support.range.e} R: {support.range.r}</p>
-                    <p>{support.notes}</p>
-                </li>
-            ))}
-        </ul>
+        <form>
+            <label htmlFor="opponent">Opponent</label>
+            <select
+                id="opponent"
+                onChange={event => setOpponent(event.target.value)}
+                onBlur={event => setOpponent(event.target.value)}
+            >
+                {supports.map((support) =>
+                    <option
+                        value={support.name}
+                        key={support.name}
+                    >
+                        {support.name}
+                    </option>
+                )}
+            </select>
+        </form>
+          <div>
+              <h2>{opponentObject.name}</h2>
+              <h3>MS: {opponentObject.ms}</h3>
+              <p>AA: {opponentObject.range.aa} Q: {opponentObject.range.q} W: {opponentObject.range.w} E: {opponentObject.range.e} R: {opponentObject.range.r}</p>
+              <p>{opponentObject.notes}</p>
+          </div>
+        {/*<ul>*/}
+        {/*    {supports.map((support) => (*/}
+        {/*        <li key={support.name}>*/}
+        {/*            <h2>{support.name}</h2>*/}
+        {/*            <h3>MS: {support.ms}</h3>*/}
+        {/*            <p>AA: {support.range.aa} Q: {support.range.q} W: {support.range.w} E: {support.range.e} R: {support.range.r}</p>*/}
+        {/*            <p>{support.notes}</p>*/}
+        {/*        </li>*/}
+        {/*    ))}*/}
+        {/*</ul>*/}
       </main>
-  )
+    )
 }
 
 export async function getStaticProps() {
