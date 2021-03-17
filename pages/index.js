a63973516ecd3193df7baab7ab8817a1cebbdbd2
Josh <34256170+ScienceOfSleep@users.noreply.github.com>
2021-03-17 12:51:31 -0600

    Fixed early render on opponent display

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ export default function Home({supports}) {
     const [ opponentObject, setOpponentObject] = useState([])
 
     useEffect(() => {
+        setOpponentObject([])
+
         supports.map((support) => {
             if (support.name === opponent){
                 setOpponentObject(support)
@@ -23,8 +25,8 @@ export default function Home({supports}) {
         {supports.map((support) =>{
             if (support.name === "Nami"){
                 return <div>
-                    <h3>MS: {support.ms}</h3>
-                    <p>AA: {support.range.aa} Q: {support.range.q} W: {support.range.w} E: {support.range.e} R: {support.range.r}</p>
+                    <h3>MS: {support.ms ?? '0'}</h3>
+                    <p>AA: {support.range.aa ?? '0'} Q: {support.range.q ?? '0'} W: {support.range.w ?? '0'} E: {support.range.e ?? '0'} R: {support.range.r ?? '0'}</p>
                 </div>
             }
         })}
@@ -32,6 +34,7 @@ export default function Home({supports}) {
             <label htmlFor="opponent">Opponent</label>
             <select
                 id="opponent"
+                defaultValue="Swain"
                 onChange={event => setOpponent(event.target.value)}
                 onBlur={event => setOpponent(event.target.value)}
             >
@@ -45,17 +48,19 @@ export default function Home({supports}) {
                 )}
             </select>
         </form>
-          <div>
-              <h2>{opponentObject.name}</h2>
-              <h3>MS: {opponentObject.ms}</h3>
-              <p>AA: {opponentObject.range.aa} Q: {opponentObject.range.q} W: {opponentObject.range.w} E: {opponentObject.range.e} R: {opponentObject.range.r}</p>
-              <p>{opponentObject.notes}</p>
-          </div>
+          {opponentObject.length === 0 ? <h2>Opponent</h2> :
+              <div>
+                  <h2>{opponentObject.name ?? '0'}</h2>
+                  <h3>MS: {opponentObject.ms ?? '0'}</h3>
+                  <p>AA: {opponentObject.range.aa || '0'} Q: {opponentObject.range.q ?? '0'} W: {opponentObject.range.w} E: {opponentObject.range.e} R: {opponentObject.range.r}</p>
+                  <p>{opponentObject.notes}</p>
+              </div>
+          }
       </main>
     )
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
     const { db } = await connectToDatabase();
     const supports = await db
         .collection("Supports")
